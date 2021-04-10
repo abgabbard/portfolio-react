5a4008c659b051f731d780b7184f344836a55a4a
Glimpz <abgabbard@gmail.com>
2021-04-10 13:53:21 -0700

    updated closing remarks

---
+++ portfolio/src/components/pages/Closing.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Closing() {
+  return (
+    <div>
+      <h1 className="text-center">Closing Remarks</h1>
+      <p>
+        Closing Remarks As you can see from my work, I believe the most
+        important solution is the human solution. I have been called a Solutions
+        Engineer, a Senior Sales Engineer, or even an Onbaording Team Lead, but
+        I am human just like you. And I care about helping people achieve their
+        vision and goals. Most importantly, I am a people person. I am fun to be
+        around. I am energetic, passionate, and encouraging. I want to see you
+        do your best work right alongside me doing mine. I enjoy learning and I
+        enjoy helping others learn. What can we accomplish together? Below you
+        will find a link to my Resume. Please share it as you see fit. Thank
+        you!
+      </p>
+    </div>
+  );
+}
+
+export default Closing;
