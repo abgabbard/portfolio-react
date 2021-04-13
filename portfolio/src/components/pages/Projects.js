3398a02e72bba11a37a386b7ebbb2c685901f6da
Glimpz <abgabbard@gmail.com>
2021-04-12 20:30:03 -0700

    projects page now functional

--- portfolio/src/components/pages/Projects.js
+++ portfolio/src/components/pages/Projects.js
@@ -1,36 +1,106 @@
-import React from 'react'
+import React from "react";
 
-const Projects = (props) => {
-    return (
-        <div>
-            <section class="content-section" id="portfolio">
-    <div class="container">
-      <div class="content-section-heading text-center">
-        <h3 class="text-secondary mb-0">Portfolio</h3>
-        <h2 class="mb-5">Recent Projects</h2>
-      </div>
-      <div class="row no-gutters">
-            {props.project.map(item => (
-                <div class="col-lg-6">
-                <a class="portfolio-item" href="#!">
-                  <div class="caption">
-                    <div class="caption-content">
-                      <div class="h2">{item.name}</div>
-                      <p class="mb-0">{item.description}</p>
-                    </div>
-                  </div>
-                  <img class="img-fluid" src={item.image} alt=""></img>
-                </a>
-              </div>
-            ))}
-      </div>
-      </div>
-      
-      </section>
-        
+const Projects = () => {
+  return (
+    <div>
+      <div class="container">
+        <div class="content-section-heading text-center">
+          <h3 class="text-secondary mb-0">Portfolio</h3>
+          <h2 class="mb-5">Recent Projects</h2>
         </div>
+        <div class="row no-gutters">
+          <div class="col-lg-6">
+            <a class="portfolio-item" href="#!">
+              <div class="caption">
+                <div class="caption-content">
+                  <div class="h2">Devour a Burger I Dare You!</div>
+                  <p class="mb-0">
+                    The App that will land me at google!
+                  </p>
+                </div>
+              </div>
+              <img class="img-fluid" src="/assets/images/burgerThumbnail.jpg" alt="NodeExpressBurgerApp"></img>
+            </a>
+          </div>
+        {/* </div> */}
+        {/* <div class="row no-gutters"> */}
+          <div class="col-lg-6">
+            <a class="portfolio-item" href="#!">
+              <div class="caption">
+                <div class="caption-content">
+                  <div class="h2">Devour a Burger I Dare You!</div>
+                  <p class="mb-0">
+                    The App that will land me at google!
+                  </p>
+                </div>
+              </div>
+              <img class="img-fluid" src="/assets/images/burgerThumbnail.jpg" alt="NodeExpressBurgerApp"></img>
+            </a>
+          </div>
+        </div>
+        <div class="row no-gutters">
+          <div class="col-lg-6">
+            <a class="portfolio-item" href="#!">
+              <div class="caption">
+                <div class="caption-content">
+                  <div class="h2">Devour a Burger I Dare You!</div>
+                  <p class="mb-0">
+                    The App that will land me at google!
+                  </p></div>
+                </div>
+              
+              <img class="img-fluid" src="/assets/images/burgerThumbnail.jpg" alt="NodeExpressBurgerApp"></img>
+            </a>
+          </div>
+        {/* </div> */}
+        {/* <div class="row no-gutters"> */}
+          <div class="col-lg-6">
+            <a class="portfolio-item" href="#!">
+              <div class="caption">
+                <div class="caption-content">
+                  <div class="h2">Devour a Burger I Dare You!</div>
+                  <p class="mb-0">
+                    The App that will land me at google!
+                  </p>
+                </div>
+              </div>
+              <img class="img-fluid" src="/assets/images/burgerThumbnail.jpg" alt="NodeExpressBurgerApp"></img>
+            </a>
+          </div>
+        </div>
+        <div class="row no-gutters">
+          <div class="col-lg-6">
+            <a class="portfolio-item" href="#!">
+              <div class="caption">
+                <div class="caption-content">
+                  <div class="h2">Devour a Burger I Dare You!</div>
+                  <p class="mb-0">
+                    The App that will land me at google!
+                  </p>
+                </div>
+              </div>
+              <img class="img-fluid" src="/assets/images/burgerThumbnail.jpg" alt="NodeExpressBurgerApp"></img>
+            </a>
+          </div>
+        {/* </div> */}
+        {/* <div class="row no-gutters"> */}
+          <div class="col-lg-6">
+            <a class="portfolio-item" href="#!">
+              <div class="caption">
+                <div class="caption-content">
+                  <div class="h2">Devour a Burger I Dare You!</div>
+                  <p class="mb-0">
+                    The App that will land me at google!
+                  </p>
+                </div>
+              </div>
+              <img class="img-fluid" src="/assets/images/burgerThumbnail.jpg" alt="NodeExpressBurgerApp"></img>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-    )
-}
-
-export default Projects
+export default Projects;
