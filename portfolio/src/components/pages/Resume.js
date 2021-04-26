ceae19352dd7feb39e1fb50a61f505c71e28a3b1
Glimpz <abgabbard@gmail.com>
2021-04-25 20:21:09 -0700

    updated resume and link

--- portfolio/src/components/pages/Resume.js
+++ portfolio/src/components/pages/Resume.js
@@ -5,7 +5,7 @@ function Resume() {
     <div>
       <section class="callout">
     <div class="container text-center">
-      <a class="btn btn-primary btn-xl" href="https://abgabbard.github.io/portfolio2021/GabbardAndrew-Resume-April2021.pdf">Resume (PDF)</a>
+      <a class="btn btn-primary btn-xl" href="https://abgabbard.github.io/portfolio2021/Gabbard%20Resume%202021.pdf">Resume (PDF)</a>
     </div>
   </section>
     </div>
